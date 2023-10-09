e3f0caf06b6fb23904a2399b69a68ccd2b02b5a3
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2023-10-09 18:37:41 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,21 +1,13 @@
-from django.urls import path
-from django.http import HttpResponse
-from django.conf import settings
-from django.conf.urls.static import static
-from flask import Flask
+from flask import Flask, render_template, request
 
-# Define a Flask app within Django
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
-@flask_app.route('/flask/')
-def flask_view():
-    return "This is a Flask view within Django."
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    user_input = ''
+    if request.method == 'POST':
+        user_input = request.form.get('user_input')
+    return render_template('index.html', user_input=user_input)
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('django/', lambda request: HttpResponse("This is a Django view.")),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if __name__ == '__main__':
+    app.run(debug=True)
