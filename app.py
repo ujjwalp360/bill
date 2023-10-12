040bb23fd906476ef3cc29b1a05fee86cca5aba7
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2023-10-12 12:50:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+ 
+st.title("Wait bro✋🏻😎")
 
+# Read the PDF file in binary mode
+#with open("expt_5.pdf", "rb") as pdf_file:
+#    pdf_bytes = pdf_file.read()
+
+# Provide a download button for the PDF
+#if st.button("Download PDF"):
+#    st.markdown("Downloading PDF file...")
+#    st.download_button(label="Click here if the download doesn't start", data=pdf_bytes, file_name="sample.pdf", key="pdf-download-button")
+st.markdown("you will get link here 👇🏻")
+#st.markdown("https://c6h12o6.streamlit.app
