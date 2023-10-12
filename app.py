204281a07b0a184c9591b2757df16510c1b8aaad
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2023-10-12 11:07:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-import streamlit as st
-import requests
 
-st.title("Streamlit App")
-
-if st.button("Authenticate with Google"):
-    # Redirect to your server for the OAuth2 authentication process
-    auth_server_url = 'https://msvtidyedlqudhixir8emj.streamlit.app/oauth2callback'
-    response = requests.get(auth_server_url)
-    st.write("Authentication initiated. Please follow the link for OAuth2 authorization.")
-
-token = st.text_input("Enter Access Token")
-if st.button("Use Access Token"):
-    # You can now use the provided access token to interact with Google Drive.
-    st.write(f"Access token: {token}")
