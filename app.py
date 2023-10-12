f90bbce41503f8a006cc0d3666266231f677e0d7
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2023-10-12 06:11:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 # Authenticate using the JSON credentials file you downloaded
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
+st.rerun()
 drive = GoogleDrive(gauth)
 
 # Name of the folder you want to create
